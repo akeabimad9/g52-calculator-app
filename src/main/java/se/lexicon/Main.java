fbbb885deb28501de92dfe7d49dfb325290b0c95
Imad <akeabimad9@gmail.com>
2024-10-11 16:16:11 +0200

    Console calculator App

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,17 +1,23 @@
 package se.lexicon;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        // ClassName.methodName();
+        ConsoleCalculator.runCalculator();
+        try {
+            System.out.println(MathOperations.divide(10, 0));
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
         }
+
+        /*System.out.println(MathOperations.add(1, 8, 1));
+
+
+
+        System.out.println(MathOperations.subtract(1, 2));
+         */
+
     }
 }
